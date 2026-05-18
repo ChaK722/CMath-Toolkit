f5b93dd1f54f2b520ecdbbd0fc110d3a170d30b5
yeung <yeungchakwing@gmail.com>
2026-05-18 16:19:18 -0400

    Add LU decomposition, determinant, solve, jacobian, and newton_raphson
    Complete module 2 (numerical linear algebra) and module 3 (calculus).
    Fix missing std:: prefixes on vector and move. Add square matrix
    validation to lu_decomposition, determinant, and solve.

--- include/matrix.h
+++ include/matrix.h
@@ -1,7 +1,9 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <cmath>
 #include <concepts>
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <stdexcept>
@@ -13,7 +15,7 @@ concept Number = std::is_arithmetic_v<T>;
 template <Number T>
 class Matrix {
   int row, col;
-  vector<T> data;
+  std::vector<T> data;
 
  public:
   Matrix(int row, int col) : row(row), col(col) { data.resize(row * col, T{}); }
@@ -21,7 +23,7 @@ class Matrix {
   Matrix(const Matrix& other)
       : row(other.row), col(other.col), data(other.data) {};
   Matrix(Matrix&& other) noexcept
-      : row(other.row), col(other.col), data(move(other.data)) {};
+      : row(other.row), col(other.col), data(std::move(other.data)) {};
 
   Matrix& operator=(const Matrix& other) {
     if (this == &other) return *this;
@@ -35,7 +37,7 @@ class Matrix {
     if (this == &other) return *this;
     row = other.row;
     col = other.col;
-    data = move(other.data);
+    data = std::move(other.data);
     return *this;
   }
 
@@ -63,7 +65,7 @@ class Matrix {
   Matrix& operator*=(const Matrix& other) {
     if (col != other.row)
       throw std::invalid_argument("Matrix dimensions do not match");
-    vector<T> newData(row * other.col, T{});
+    std::vector<T> newData(row * other.col, T{});
     for (int i = 0; i < row; i++) {
       for (int j = 0; j < other.col; j++) {
         for (int k = 0; k < col; k++) {
@@ -72,7 +74,7 @@ class Matrix {
         }
       }
     }
-    data = move(newData);
+    data = std::move(newData);
     col = other.col;
     return *this;
   }
@@ -110,17 +112,25 @@ class Matrix {
     return result;
   }
 
+  static Matrix identity(int n) {
+    Matrix result(n, n);
+    for (int i = 0; i < n; i++) {
+      result.data[i * n + i] = 1;
+    }
+    return result;
+  }
+
   int get_row() const { return row; }
   int get_col() const { return col; }
 
   friend std::ostream& operator<<(std::ostream& os, const Matrix& matrix) {
-      for(int i = 0; i < matrix.row; i++) {
-        for(int j = 0; j < matrix.col; j++) {
-          os << matrix.data[i * matrix.col + j] << " ";
-        }
-        os << std::endl;
+    for (int i = 0; i < matrix.row; i++) {
+      for (int j = 0; j < matrix.col; j++) {
+        os << matrix.data[i * matrix.col + j] << " ";
       }
-      return os;
+      os << std::endl;
+    }
+    return os;
   }
 
   bool operator==(const Matrix& other) const {
@@ -135,17 +145,17 @@ class Matrix {
     return true;
   }
 
-  bool operator!=(const Matrix& other) const {
-    return !(*this == other);
-  }
+  bool operator!=(const Matrix& other) const { return !(*this == other); }
 
   T& operator()(int i, int j) {
-    if(i >= row || j >= col || i < 0 || j < 0) throw std::out_of_range("Index out of range");
+    if (i >= row || j >= col || i < 0 || j < 0)
+      throw std::out_of_range("Index out of range");
     return data[i * col + j];
   }
 
   const T& operator()(int i, int j) const {
-    if(i >= row || j >= col || i < 0 || j < 0) throw std::out_of_range("Index out of range");
+    if (i >= row || j >= col || i < 0 || j < 0)
+      throw std::out_of_range("Index out of range");
     return data[i * col + j];
   }
 
@@ -159,8 +169,112 @@ class Matrix {
     return result;
   }
 
+  std::pair<Matrix, Matrix> lu_decomposition() const {
+    if (row != col)
+      throw std::invalid_argument("Matrix must be square for LU decomposition");
+    Matrix l(row, col);
+    Matrix u(row, col);
 
-  
-}
+    for (int i = 0; i < row; i++) {
+      l(i, i) = 1;
+      for (int j = i; j < col; j++) {
+        u(i, j) = (*this)(i, j);
+        for (int k = 0; k < i; k++) {
+          u(i, j) -= l(i, k) * u(k, j);
+        }
+      }
+      for (int j = i + 1; j < col; j++) {
+        l(j, i) = (*this)(j, i);
+        for (int k = 0; k < i; k++) {
+          l(j, i) -= l(j, k) * u(k, i);
+        }
+        l(j, i) /= u(i, i);
+      }
+    }
+    return std::make_pair(l, u);
+  }
+
+  T determinant() const {
+    if (row != col)
+      throw std::invalid_argument("Matrix must be square to compute determinant");
+    T det = 1;
+    auto [L, U] = lu_decomposition();
+    for (int i = 0; i < row; ++i) {
+      det *= U(i, i);
+    }
+    return det;
+  }
+
+  Matrix solve(const Matrix& b) const {
+    if (row != col)
+      throw std::invalid_argument("Matrix must be square to solve linear system");
+    if (b.get_row() != row || b.get_col() != 1)
+      throw std::invalid_argument("b must be a column vector matching matrix dimensions");
+    std::pair<Matrix, Matrix> lu = lu_decomposition();
+    Matrix L = lu.first;
+    Matrix U = lu.second;
+
+    Matrix y(col, 1);
+    for (int i = 0; i < col; i++) {
+      y(i, 0) = b(i, 0);
+      for (int j = 0; j < i; j++) {
+        y(i, 0) -= L(i, j) * y(j, 0);
+      }
+    }
+
+    Matrix x(col, 1);
+    for (int i = col - 1; i >= 0; i--) {
+      x(i, 0) = y(i, 0);
+      for (int j = i + 1; j < col; j++) {
+        x(i, 0) -= U(i, j) * x(j, 0);
+      }
+      x(i, 0) /= U(i, i);
+    }
+    return x;
+  }
+
+  static Matrix jacobian(const std::function<Matrix(const Matrix&)>& func,
+                         const Matrix& x) {
+    int n = x.get_row();
+    Matrix fx = func(x);
+    int m = fx.get_row();
+    Matrix J(m, n);
+    T h = T(1e-6);
+
+    for (int j = 0; j < n; j++) {
+      Matrix x_plus = x;
+      Matrix x_minus = x;
+      x_plus(j, 0) += h;
+      x_minus(j, 0) -= h;
+
+      Matrix f_plus = func(x_plus);
+      Matrix f_minus = func(x_minus);
+
+      for (int i = 0; i < m; i++) {
+        J(i, j) = (f_plus(i, 0) - f_minus(i, 0)) / (T(2) * h);
+      }
+    }
+    return J;
+  }
+
+  static Matrix newton_raphson(
+      const std::function<Matrix(const Matrix&)>& func,
+      const Matrix& x0, T tol = T(1e-6), int max_iter = 100) {
+    Matrix x = x0;
+
+    for (int iter = 0; iter < max_iter; iter++) {
+      Matrix f = func(x);
+      T norm = 0;
+      for (int i = 0; i < f.get_row(); i++) norm += f(i, 0) * f(i, 0);
+      norm = std::sqrt(norm);
+      if (norm < tol) break;
+
+      Matrix J = jacobian(func, x);
+      Matrix dx = J.solve(f * T(-1));
+      x = x + dx;
+    }
+    return x;
+  }
+};
 
 #endif  // MATRIX_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
-#include "iostream"
+#include <iostream>
+#include "matrix.h"
+
 using namespace std;
 
-int main(){
-    cout << "Hello World!";
+int main() {
+    cout << "--- Linear System Solver (Ax = b) Test ---" << endl;
+
+    // 1. 初始化系数矩阵 A (3x3)
+    Matrix<double> A(3, 3);
+    A(0,0) = 2; A(0,1) = 1; A(0,2) = 1;
+    A(1,0) = 4; A(1,1) = 3; A(1,2) = 3;
+    A(2,0) = 8; A(2,1) = 7; A(2,2) = 9;
+    
+    // 2. 初始化结果列向量 b (3x1)
+    Matrix<double> b(3, 1);
+    b(0,0) = 4;
+    b(1,0) = 10;
+    b(2,0) = 24;
+
+    cout << "Matrix A:\n" << A;
+    cout << "Vector b:\n" << b;
+
+    // 3. 求解 Ax = b
+    try {
+        Matrix<double> x = A.solve(b);
+        cout << "\nSolution vector x:\n" << x;
+        
+        // 4. 验算: A * x 是否等于 b
+        cout << "\nVerification (A * x should equal b):\n" << A * x;
+        
+    } catch (const exception& e) {
+        cerr << "Error solving system: " << e.what() << endl;
+    }
+
+    return 0;
 }
